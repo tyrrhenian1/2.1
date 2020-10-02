d495b2ded4ccc78208c8710f7b8c9d3968632629
Артем Лазарчик <lazarchik_2000@mail.ru>
2020-10-02 23:45:46 +0300

    Добавьте файлы проекта.

---
+++ 2.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+                {
+                Console.Write("Введите значение x:");
+                double x = double.Parse(Console.ReadLine());
+                Console.Write("Введите значение y:");
+                double y = double.Parse(Console.ReadLine());
+                bool i = (x * x + y * y <= 4) && !(x * x + y * y + 2 * x <= 0);     
+                Console.WriteLine(i);
+
+
+
+
+
+                }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+            
+    }
+}
+
